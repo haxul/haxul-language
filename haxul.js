933de88c98e9555ac86f1474eac2acdab8381438
starodubov <s.s.starodubov@yandex.com>
2021-03-21 13:48:11 +0400

    gfd

---
+++ haxul.js
@@ -0,0 +1,13 @@
+const assert = require("assert")
+
+class Haxul {
+    eval(exp) {
+        if (isNumber(exr)) return exp;
+
+        throw "Unimplemented";
+    }
+}
+
+
+const isNumber = ele => typeof ele === "number"
+const isString = ele => typeof ele === "string" &&
