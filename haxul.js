a8ecb4eefefdd45d62b545f448cfa78b898a93b8
starodubov <s.s.starodubov@yandex.com>
2021-03-21 13:55:40 +0400

    implement numbers and strings

--- haxul.js
+++ haxul.js
@@ -2,12 +2,20 @@ const assert = require("assert")
 
 class Haxul {
     eval(exp) {
-        if (isNumber(exr)) return exp;
-
+        if (isNumber(exp)) return exp;
+        if (isString(exp)) return exp.slice(1, -1);
         throw "Unimplemented";
     }
 }
 
 
 const isNumber = ele => typeof ele === "number"
-const isString = ele => typeof ele === "string" &&
+const isString = ele => typeof ele === "string" && ele[0] === '"' && ele.slice(-1) === '"'
+
+
+const haxul = new Haxul();
+
+assert.strictEqual(haxul.eval(1), 1)
+assert.strictEqual(haxul.eval('"haxul"'), "haxul")
+
+console.log("tests are passed")
