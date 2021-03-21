2a3f9a8f269b431a3534c53ea5059d1b65f30b1d
starodubov <s.s.starodubov@yandex.com>
2021-03-21 15:37:25 +0400

    implement numbers and strings

---
+++ environment.js
@@ -0,0 +1,20 @@
+module.exports = class Environment {
+
+    record
+
+    constructor(record = {}) {
+        this.record = record
+    }
+
+    define(name, value) {
+        this.record[name] = value
+        return value
+    }
+
+    lookup(name) {
+        if (!this.record.hasOwnProperty(name)){
+            throw new ReferenceError(`Variable "${name}" is not defined`)
+        }
+        return this.record[name]
+    }
+}
--- haxul.js
+++ haxul.js
@@ -1,9 +1,32 @@
 const assert = require("assert")
+const Environment = require("./environment")
 
 class Haxul {
-    eval(exp) {
+
+    globalEnv
+
+    constructor(global = new Environment({
+        true: true,
+        false: false,
+        null: null
+    })) {
+        this.globalEnv = global
+    }
+
+    eval(exp ,env = this.globalEnv) {
         if (isNumber(exp)) return exp;
-        if (isString(exp)) return exp.slice(1, -1);
+        if (isString(exp)) return exp.slice(1, -1)
+        if (exp[0] === "+") return this.eval(exp[1]) + this.eval(exp[2])
+        if (exp[0] === "*") return this.eval(exp[1]) * this.eval(exp[2])
+
+        if (exp[0] === "var") {
+            const [_, name, value] = exp
+            return env.define(name, this.eval(value))
+        }
+
+        if (isVariableName(exp)) {
+            return env.lookup(exp)
+        }
         throw "Unimplemented";
     }
 }
@@ -11,11 +34,24 @@ class Haxul {
 
 const isNumber = ele => typeof ele === "number"
 const isString = ele => typeof ele === "string" && ele[0] === '"' && ele.slice(-1) === '"'
-
+const isVariableName = ele => typeof ele === "string" && /^[a-zA-Z][a-zA-Z0-9_]*$/.test(ele)
 
 const haxul = new Haxul();
+
 
 assert.strictEqual(haxul.eval(1), 1)
 assert.strictEqual(haxul.eval('"haxul"'), "haxul")
 
-console.log("tests are passed")
+// math
+assert.strictEqual(haxul.eval(["+", 1, 2]), 3)
+assert.strictEqual(haxul.eval(["+", ["+", 1, 2], 7]), 10)
+assert.strictEqual(haxul.eval(["*", ["*", 1, 2], 7]), 14)
+
+// variables
+assert.strictEqual(haxul.eval(["var", "x", 10]), 10)
+assert.strictEqual(haxul.eval("x"), 10)
+assert.strictEqual(haxul.eval("null"), null)
+assert.strictEqual(haxul.eval(["var", "isUser", "true"]), true)
+assert.strictEqual(haxul.eval(["var", "z", ["*", 2, 2]]), 4)
+
+console.log("tests passed")
